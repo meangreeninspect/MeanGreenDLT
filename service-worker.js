bf448233b9d6b19c6f077038f3ca94a54cb5bab5
meangreeninspect <meangreeninspect@gmail.com>
2026-07-19 13:51:42 -0500

    Fix ipad buttons

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,17 @@
-const CACHE_NAME = "mean-green-dlt-v4-ipad-pdf-fix";
-const LOCAL_ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./Mean_Green_Logo.png",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
+const CACHE_NAME = "mean-green-dlt-v6-reliable-buttons";
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache =>
-      Promise.all(LOCAL_ASSETS.map(asset => cache.add(asset).catch(() => null)))
-    )
-  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      fetch(event.request, { cache: "no-store" })
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put("./index.html", copy));
-          return response;
-        })
-        .catch(() => caches.match("./index.html"))
-    );
-    return;
-  }
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request, { cache: "no-store" }));
 });
